9b9fe4bf09458fcfc779ce6e4e3a1afc157219b5
AliciaJz <aliciajuarez.dev@gmail.com>
2019-03-24 20:30:41 -0600

    hasta aqui aun funciona

--- src/App.js
+++ src/App.js
@@ -2,6 +2,21 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import gql from 'graphql-tag';
+import { graphql, compose } from 'react-apollo';
+
+const ListClases = gql `
+  query {
+    listClases {
+      items {
+        id
+        nombreClase
+        Ponente
+      }
+    }
+  }
+`
+
 class App extends Component {
   render() {
     return (
@@ -9,20 +24,26 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Cambia <code>src/App.js</code> and save to reload.
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          {
+            this.props.clases.map((item, i) => {
+              return <p key={i}> {item.id}</p>
+             })
+          }
         </header>
       </div>
     );
   }
 }
 
-export default App;
+export default compose(
+  graphql(ListClases, {
+    options: {
+      fetchPolicy: 'cache-and-network'
+    },
+    props: props => ({
+      clases: props.data.listClases ? props.data.listClases.items : []
+    })
+  })
+)(App);
