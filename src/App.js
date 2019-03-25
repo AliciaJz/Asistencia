474bbd3279dcc285871e68b59457bbf30f72ecd8
AliciaJz <aliciajuarez.dev@gmail.com>
2019-03-24 20:37:58 -0600

    react de inicio

--- src/App.js
+++ src/App.js
@@ -2,21 +2,6 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import gql from 'graphql-tag';
-import { graphql, compose } from 'react-apollo';
-
-const ListClases = gql `
-  query {
-    listClases {
-      items {
-        id
-        nombreClase
-        Ponente
-      }
-    }
-  }
-`
-
 class App extends Component {
   render() {
     return (
@@ -24,26 +9,20 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Cambia <code>src/App.js</code> and save to reload.
+            Edit <code>src/App.js</code> and save to reload.
           </p>
-          {
-            this.props.clases.map((item, i) => {
-              return <p key={i}> {item.id}</p>
-             })
-          }
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
         </header>
       </div>
     );
   }
 }
 
-export default compose(
-  graphql(ListClases, {
-    options: {
-      fetchPolicy: 'cache-and-network'
-    },
-    props: props => ({
-      clases: props.data.listClases ? props.data.listClases.items : []
-    })
-  })
-)(App);
+export default App;
